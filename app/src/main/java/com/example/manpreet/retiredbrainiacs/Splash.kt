413e6fe4fe04241c12f618058a6243688e4613ae
Manpreet <manpreetanything@gmail.com>
2018-07-03 11:11:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manpreet/retiredbrainiacs/Splash.kt
@@ -0,0 +1,12 @@
+package com.example.manpreet.retiredbrainiacs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
